393ac16b3408a349970f3452b0542fbd1969a771
housong12590 <304536797@qq.com>
2015-07-16 23:12:42 +0800

    7-16 提交文档

---
+++ TZ_云烟/gen/com/ws/util/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.ws.util;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ TZ_云烟/src/com/ws/toos/util/MainActivity.java
@@ -0,0 +1,18 @@
+package com.ws.toos.util;
+
+import com.ws.util.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	
+}
