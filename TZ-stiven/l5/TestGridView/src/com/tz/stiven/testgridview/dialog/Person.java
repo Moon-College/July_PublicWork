42400e30ecb695dc8bac93d5a8327802d1eeb8b5
ZJQXT <114442034@qq.com>
2015-07-24 20:01:12 +0800

    第五次课的作业，1、listview；2、gridView
    Signed-off-by: ZJQXT <114442034@qq.com>

---
+++ TZ-stiven/l5/TestGridView/src/com/tz/stiven/testgridview/dialog/Person.java
@@ -0,0 +1,14 @@
+package com.tz.stiven.testgridview.dialog;
+
+/** 
+ * @author 作者 tz_stiven QQ:114442034 
+ * @version 创建时间：2015年7月23日 下午8:09:12 
+ * 类说明 
+ */
+public class Person {
+	public int icon;
+	public String name;
+	public String describe;
+	public int numString;
+	public int foodImage;
+}
