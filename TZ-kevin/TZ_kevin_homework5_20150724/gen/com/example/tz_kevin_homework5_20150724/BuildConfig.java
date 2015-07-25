7c1a5b27b39b8bd6d9deee5c4bc34a17df69116a
kevin <935735753@qq.com>
2015-07-25 17:46:12 +0800

    delet
    格式没有传对

--- TZ-kevin/TZ_kevin_homework5_20150724/gen/com/example/tz_kevin_homework5_20150724/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.tz_kevin_homework5_20150724;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
