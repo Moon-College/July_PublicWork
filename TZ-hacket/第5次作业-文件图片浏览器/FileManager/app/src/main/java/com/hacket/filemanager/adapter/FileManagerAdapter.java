1b3276dc769b5ecf0fc5bc807e57b22598175391
hacket <zeng_fansheng@sina.com>
2015-07-28 01:14:33 +0800

    第六次作业：退出对话框，长按popupwindow

--- TZ-hacket/第5次作业-文件图片浏览器/FileManager/app/src/main/java/com/hacket/filemanager/adapter/FileManagerAdapter.java
+++ TZ-hacket/第5次作业-文件图片浏览器/FileManager/app/src/main/java/com/hacket/filemanager/adapter/FileManagerAdapter.java
@@ -27,7 +27,7 @@ public class FileManagerAdapter extends DefaultAdapter<FileInfo> {
             holder = new FileManagerViewHolder(mContext);
         } else {
             if (holder instanceof BaseViewHolder) {
-                holder = (BaseViewHolder<FileInfo>) convertView.getTag();
+                holder = (BaseViewHolder<FileInfo>) convertView.getTagwrap_content();
             } else {
                 holder = new FileManagerViewHolder(mContext);
             }
