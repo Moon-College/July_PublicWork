3fef3af3a4615b255a067581038de4a7ce37249e
zomb1eAlex <318084296@qq.com>
2015-08-02 06:07:40 +0800

    Tz_Alex
    Tz_Alex

---
+++ TZ_Alex/Tz_Alex第八次作业(自定义音量调控)8-2/src/com/tz/MainActivity.java
@@ -0,0 +1,20 @@
+package com.tz;
+
+import com.example.tzclass9_customvolumn.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+	
+
+}
