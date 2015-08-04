80e609d0716473f1dd05e9523a60cdf6dcf203ba
zhangqufa <afa1989@126.com>
2015-08-04 11:40:55 +0800

    FileBrowser作业

---
+++ TZ-小D/潭州-小D  FileBrowser作业/FileBrowse/app/src/main/java/com/tz/browse/adapter/MyFileAdapter.java
@@ -0,0 +1,100 @@
+package com.tz.browse.adapter;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.tz.browse.bean.MyFile;
+import com.tz.filebrowse.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Çü·¢ on 2015/8/3.
+ */
+public class MyFileAdapter extends BaseAdapter {
+    private final Context context;
+    private List<MyFile> list = new ArrayList<>();
+
+    public MyFileAdapter(List<MyFile> list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return list.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        MyFile myFile = list.get(position);
+        View view = convertView;
+        final ViewHolder holder;
+        if (convertView == null) {
+            holder = new ViewHolder();
+            view = View.inflate(context, R.layout.item_file, null);
+            holder.iv_icon = (ImageView) view.findViewById(R.id.iv_icon);
+            holder.tv_name = (TextView) view.findViewById(R.id.tv_name);
+            view.setTag(holder);
+        } else {
+            holder = (ViewHolder) view.getTag();
+        }
+        if (myFile.getIcon() == null) {
+
+            ImageLoadAsyncTask task = new ImageLoadAsyncTask();
+            task.execute(myFile.getPath(), position + "");
+        } else {
+            holder.iv_icon.setImageBitmap(myFile.getIcon());
+        }
+
+        holder.tv_name.setText(myFile.getName());
+
+        return view;
+    }
+
+    public class ViewHolder {
+        public ImageView iv_icon;
+        private TextView tv_name;
+    }
+
+
+    class ImageLoadAsyncTask extends AsyncTask<String, Void, Void> {
+
+        @Override
+        protected Void doInBackground(String... params) {
+            String path = params[0];
+            String position = params[1];
+            try {
+                Bitmap bitmap = BitmapFactory.decodeFile(path);
+                list.get(Integer.parseInt(position)).setIcon(bitmap);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            notifyDataSetChanged();
+        }
+    }
+}
---
+++ TZ-小D/潭州-小D  FileBrowser作业/FileBrowse/app/src/main/java/com/tz/browse/base/BaseActivity.java
@@ -0,0 +1,78 @@
+package com.tz.browse.base;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.tz.filebrowse.R;
+
+/**
+ * Created by 屈发 on 2015/8/3.
+ */
+public abstract class BaseActivity extends Activity {
+    private LinearLayout ly_content;
+    private TextView tv_titlebar_left, tv_titlebar_right, tv_titlebar_center;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.common_title_bar);
+        /**
+         * 1.BaseActivity---》common_title.xml
+         * 2.xxx_layout.addView(view);
+         * 3.添加一个方法：setContentLayout(View v);
+         * 				setContentLayout(int resId);
+         * 4.再处理返回按钮、标题设置等操作。
+         */
+        ly_content = (LinearLayout) findViewById(R.id.ly_content);
+        tv_titlebar_left = (TextView) findViewById(R.id.tv_titlebar_left);
+        tv_titlebar_right = (TextView) findViewById(R.id.tv_titlebar_right);
+        tv_titlebar_center = (TextView) findViewById(R.id.tv_titlebar_center);
+
+    }
+
+
+    protected void setContentLayout(int resId) {
+        View contentView = View.inflate(getApplicationContext(), resId, null);
+        ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        contentView.setLayoutParams(lp);
+        if (ly_content != null) {
+            ly_content.addView(contentView);
+        }
+    }
+
+    protected TextView getTitlebarLeft() {
+        return tv_titlebar_left;
+    }
+
+    protected TextView getTitlebarRight() {
+        return tv_titlebar_right;
+    }
+
+    protected TextView getTitlebarCenter() {
+        return tv_titlebar_center;
+    }
+
+    protected void hideTitlebarLeft() {
+        if (tv_titlebar_left != null) {
+            tv_titlebar_left.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    protected void hideTitlebarRight() {
+        if (tv_titlebar_right != null) {
+            tv_titlebar_right.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    protected void setBarTitle(String title) {
+        if (tv_titlebar_center != null) {
+            tv_titlebar_center.setText(title);
+        }
+    }
+
+}
