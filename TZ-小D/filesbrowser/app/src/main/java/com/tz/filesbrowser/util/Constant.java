0d0fcc9105d72441729513be751a277a306587ea
zhangqufa <afa1989@126.com>
2015-08-05 17:17:22 +0800

    d

--- TZ-小D/filesbrowser/app/src/main/java/com/tz/filesbrowser/util/Constant.java
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (c) 2015, ParkTech Corporation, All Rights Reserved
- */
-package com.tz.filesbrowser.util;
-
-public final class Constant {
-    public static final String TAG = "debugTag";
-}
